c529c6c993ca00ba54d96b64587a0e9ff42583c1
Eunji Park <amu601@naver.com>
2025-03-24 00:11:15 +0900

    2주차 과제 최종

--- 앨리스_박은지/2주차/미션2/src/App.tsx
+++ 앨리스_박은지/2주차/미션2/src/App.tsx
@@ -1,16 +1,11 @@
+import { ThemeProvider } from "./useContext/ContextPage";
+import ContextPage from "./useContext/ContextPage";
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold text-gray-800 mb-4">
-          Dark Mode Example
-        </h1>
-        <p className="text-gray-600">
-          This is a sample text with Tailwind CSS styling.
-        </p>
-      </div>
-    </div>
+    <ThemeProvider>
+      <ContextPage />
+    </ThemeProvider>
   );
 }
