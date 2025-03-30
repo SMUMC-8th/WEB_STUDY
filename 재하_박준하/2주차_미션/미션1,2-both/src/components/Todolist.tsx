d25090654871697575a47be864e351d3c308d2e4
jeha0714 <whrydus0714@gmail.com>
2025-03-30 19:49:17 +0900

    CHORE: make CSS great Again

--- 재하_박준하/2주차_미션/미션1,2-both/src/components/Todolist.tsx
+++ 재하_박준하/2주차_미션/미션1,2-both/src/components/Todolist.tsx
@@ -3,22 +3,22 @@ import { useTodo } from "../context/TodoContext";
 import { workType } from "./Worklist";
 
 export default function Todolist() {
-	const {todos, doneTodos, completeTodo, deleteTodo} = useTodo();
+  const { todos, doneTodos, completeTodo, deleteTodo } = useTodo();
 
-	return (
-		<article className="box-border flex flex-col sm:flex-row justify-center sm:justify-around itemes-center">
-			<Worklist 
-				todoArr={todos}
-				todoFunc={completeTodo}
-				type={workType.TODO}
-				>
-			</Worklist>
-			<Worklist 
-				todoArr={doneTodos}
-				todoFunc={deleteTodo}
-				type={workType.DONE}
-				>
-			</Worklist>
-		</article>
-	);
+  return (
+    <article className="box-border flex flex-col sm:flex-row justify-center sm:justify-around itemes-center">
+      <Worklist
+        todoArr={todos}
+        todoFunc={completeTodo}
+        type={workType.TODO}
+        typeBtn={workType.TODOBTN}
+      ></Worklist>
+      <Worklist
+        todoArr={doneTodos}
+        todoFunc={deleteTodo}
+        type={workType.DONE}
+        typeBtn={workType.DONEBTN}
+      ></Worklist>
+    </article>
+  );
 }
--- 재하_박준하/2주차_미션/미션1,2-both/src/pages/TodolistPage.tsx
+++ 재하_박준하/2주차_미션/미션1,2-both/src/pages/TodolistPage.tsx
@@ -6,17 +6,18 @@ import { THEME } from "../context/ThemeProvider";
 import { TodoProvider } from "../context/TodoContext";
 
 export default function TodolistPage() {
-	const {theme} = useTheme();
-	
-	return (
-        <section 
-		  className={`w-[50%] break-all
-			${theme === THEME.LIGHT ? " bg-white" : " bg-gray-500"}`}>
-			<Navbar></Navbar>
-			<TodoProvider>
-				<InputForm></InputForm>
-				<Todolist></Todolist>
-			</TodoProvider>
-        </section>
-	)
+  const { theme } = useTheme();
+
+  return (
+    <section
+      className={`w-[50%] rounded-xl break-all
+			${theme === THEME.LIGHT ? " bg-white" : " bg-gray-500"}`}
+    >
+      <Navbar></Navbar>
+      <TodoProvider>
+        <InputForm></InputForm>
+        <Todolist></Todolist>
+      </TodoProvider>
+    </section>
+  );
 }
