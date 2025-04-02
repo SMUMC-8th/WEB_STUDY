0af21ac964905cc086227c29dce11a6fc9d7c93c
yujin5959 <jinjin17104@naver.com>
2025-04-02 11:07:43 +0900

    FEAT: 4주차 과제 내 정보 불러오기 기능 추가

--- 제이_지유진/4주차_미션/UMC-8th-mission-FE/src/components/Navbar.tsx
+++ 제이_지유진/4주차_미션/UMC-8th-mission-FE/src/components/Navbar.tsx
@@ -1,5 +1,23 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getMyInfo } from "../apis/auth.ts";
+
 export default function Navbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userName, setUserName] = useState("");
+  useEffect(() => {
+    const getData = async () => {
+      const response = await getMyInfo();
+      if (response.statusCode === 200) {
+        setIsLoggedIn(true);
+        console.log(response.data.name);
+        setUserName(response.data.name);
+      } else {
+        setIsLoggedIn(false);
+      }
+    };
+    getData();
+  }, []);
   const navigate = useNavigate();
   const handleLoginClick = () => {
     navigate("login");
@@ -18,20 +36,30 @@ export default function Navbar() {
       >
         돌려돌려LP판
       </p>
-      <div className="flex gap-4">
-        <button
-          className="rounded-[5px] text-white text-m bg-gray-900 w-[70px] cursor-pointer p-[5px]"
-          onClick={handleLoginClick}
-        >
-          로그인
-        </button>
-        <button
-          className="rounded-[5px] text-white text-m bg-pink-600  w-[70px] cursor-pointer p-[5px]"
-          onClick={handleSignupClick}
-        >
-          회원가입
-        </button>
-      </div>
+      {!isLoggedIn && (
+        <div className="flex gap-4">
+          <button
+            className="rounded-[5px] text-white text-m bg-gray-900 w-[70px] cursor-pointer p-[5px]"
+            onClick={handleLoginClick}
+          >
+            로그인
+          </button>
+          <button
+            className="rounded-[5px] text-white text-m bg-pink-600  w-[70px] cursor-pointer p-[5px]"
+            onClick={handleSignupClick}
+          >
+            회원가입
+          </button>
+        </div>
+      )}
+      {isLoggedIn && (
+        <div className="flex gap-4 text-white text-center jutify-center items-center">
+          <p>안녕하세요, {userName}님</p>
+          <button className="rounded-[5px] text-white text-m bg-pink-600  w-[70px] cursor-pointer p-[5px]">
+            로그아웃
+          </button>
+        </div>
+      )}
     </div>
   );
 }
