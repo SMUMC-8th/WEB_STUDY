db864be3a8750345afd6e8c1d8c31eef5bbcee93
minzx23 <kmjn0623@gmail.com>
2025-04-06 19:41:23 +0900

    FEAT: 4주차 미션1 완료

--- 주민_강민주/3주차_미션/미션1/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import router from './routes/routes'
-import './App.css'
-import { RouterProvider } from 'react-router-dom'
-
-function App() {
-  return <RouterProvider router={router}></RouterProvider>;
-}
-
-export default App;
