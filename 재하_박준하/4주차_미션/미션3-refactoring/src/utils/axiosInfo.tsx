f41111d5912315307e0879fdc20a52f03a402a00
jeha0714 <whrydus0714@gmail.com>
2025-04-28 11:40:58 +0900

    CHORE: 기존 미션3 제거

--- 재하_박준하/4주차_미션/미션3-refactoring/src/utils/axiosInfo.tsx
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const umcServer = axios.create({
-  baseURL: `${import.meta.env.VITE_SERVER}`,
-  timeout: 1000,
-  headers: { accept: "application/json" },
-});
